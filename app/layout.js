dacb2ea17d6548d31e08ed91bdffe96caa8c0cba
Stepan Korshukov <korshukovstepan@gmail.com>
2023-11-21 13:54:04 +0300

    fix: change meta description, add margin to the right sidebar

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import './ui/globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Lama Dev Next.js Admin Dashboard',
-  description: 'Next.js 14 Tutorial',
+  title: 'Next.js Admin Dashboard',
+  description: 'Next.js 14 Server Actions Test Project',
 }
 
 export default function RootLayout({ children }) {
